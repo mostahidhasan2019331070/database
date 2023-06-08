54f22c02df7bb53bad8d997cc9598286367624ea
mostahidhasan2019331070 <fahimmostahid70@gmail.com>
2023-06-08 23:06:27 +0600

    first commit

--- app.js
+++ app.js
@@ -30,6 +30,27 @@ app.get('/category/:id', (req, res) => {
   app.get('/post', (req, res) => {
     res.send('Welcome to the user  page!');
   });
+  app.get('/registration', (req, res) => {
+    res.send('Welcome to the registration   page!');
+  });
+  app.get('/login', (req, res) => {
+    res.send('Welcome to the login   page!');
+  });
+  app.get('/chat', (req, res) => {
+    res.send('Welcome to the chat  page!');
+  });
+  app.get('/chat/:id', (req, res) => {
+    res.send('Welcome to the chat/:id  page!');
+  });
+  app.get('/product',(res,req)=>
+  {
+    res.send('Welcome to the product  page!');
+  })
+  app.get('/query',(res,req)=>
+  {
+    res.send("well come to query page !");
+  })
+  
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
